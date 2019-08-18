c62989a208134e0cdd6ec509b31e3d12dcd60dcc
icony/精武陈真 <chen_zhen@hisuntech.com>
2019-08-18 22:35:55 +0800

    'index.js'

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: entry point
+ * @Author: icony/精武陈真
+ * @Date: 2019-08-18 22:25:32
+ * @LastEditTime: 2019-08-18 22:35:24
+ * @LastEditors: icony/精武陈真
+ */
+var express = require('express');
+var app = express();
